19fea46d5e34f217b343e45aec7d3840803420ba
Camlac <90343750+Camlac@users.noreply.github.com>
2021-09-13 07:28:43 -0500

    13/09/2021

---
+++ 13/09/21.py
@@ -0,0 +1,9 @@
+# En este script acemos operaciones basicas
+# 13 de sep 2021
+suma 5 + 2 
+resta = suma -8.5
+m1 = 32
+m2 = 64
+multiplicacion + m1 -suma 
+print (multiplicacion)
+print ("la resta dio como resultado ")
